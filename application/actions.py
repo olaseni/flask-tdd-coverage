28aee553f5b391b63fb8d3cae9d57b1bf9c3f0ba
Seni Oluwunmi <olaseni.oluwunmi@laitech.com>
2018-12-03 03:44:04 +0100

    Added: action stubs

--- application/actions.py
+++ application/actions.py
@@ -1,3 +1,54 @@
+"""
+Operations related to managing a person entity
+"""
 
 
+def add_person(first_name, last_name, email, phone, date_of_birth, address, profession, notes=None):
+    """
+    Adds a person and returns the id
+    :param first_name:
+    :param last_name:
+    :param email:
+    :param phone:
+    :param date_of_birth:
+    :param address:
+    :param profession:
+    :param notes:
+    :return:
+    """
+    return 0
 
+
+def view_person(id):
+    """
+    Returns a person from the db by id
+    :param id:
+    :return:
+    """
+    return None
+
+
+def edit_person(id, first_name, last_name, email, phone, date_of_birth, address, profession, notes=None):
+    """
+    Updates a person's details
+    :param id:
+    :param first_name:
+    :param last_name:
+    :param email:
+    :param phone:
+    :param date_of_birth:
+    :param address:
+    :param profession:
+    :param notes:
+    :return:
+    """
+    pass
+
+
+def remove_person(id):
+    """
+    Removes a person from the db by id
+    :param id:
+    :return:
+    """
+    pass
