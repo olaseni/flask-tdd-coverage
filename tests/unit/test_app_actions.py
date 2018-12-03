29e9bd8f535d5c0173cd95cadc91e8eb58f7a865
Seni Oluwunmi <olaseni.oluwunmi@laitech.com>
2018-12-03 03:57:02 +0100

    Added: unit test stubs

---
+++ tests/unit/test_app_actions.py
@@ -0,0 +1,19 @@
+"""
+Unit tests for actions
+"""
+
+
+def test_add_person():
+    pass
+
+
+def test_view_person():
+    pass
+
+
+def test_edit_person():
+    pass
+
+
+def test_remove_person():
+    pass
